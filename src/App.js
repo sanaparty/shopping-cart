c5cf3579319e298fbd02ad9e811d262c07c77d2a
ashrin <ashrin@yahoo.com>
2019-09-12 21:25:01 -0400

    adding buttons

--- src/App.js
+++ src/App.js
@@ -6,22 +6,41 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      name:'ash' 
+      name:'' ,
+      num:1
     };
   this.myfunct = this.myfunct.bind(this);
+  this.mysanka = this.mysanka.bind(this);
+  this.myfunct3 = this.myfunct3.bind(this);
   }
   myfunct() {
-    if(this.state.name=='ash')
-    this.state.name = 'srk'
-    else
-      this.state.name='ash'
+    this.state.name='sru'
+    this.state.num+=1;
+    this.forceUpdate();
+   }
+
+   mysanka() {
+    this.state.name='ash'
+    this.state.num+=1;
+     this.forceUpdate();
+  }
+
+   myfunct3() {
+    this.state.name='sne'  
+    this.state.num+=1;
     this.forceUpdate();
   }
+  
+    
+  
   render() {
     return (
       <div>
         <p>{this.state.name}</p>
+        <p>{this.state.num}</p>
         <button onClick={this.myfunct}>Clickuuu</button>
+        <button onClick={this.mysanka}>Clickuuu me twice</button>
+        <button onClick={this.myfunct3}>Clickuuu me thrice</button>
       </div>
     );
   }
